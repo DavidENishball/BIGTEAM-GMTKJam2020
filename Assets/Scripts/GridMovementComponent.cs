ab9d5b62be0ad0559551f5bc027346e15c52d31c
DESKTOP-6QRDQEC\DavidN <DavidENishball@gmail.com>
2020-07-10 20:10:04 -0400

    Setup grid component and a player character

--- Assets/Scripts/GridMovementComponent.cs
+++ Assets/Scripts/GridMovementComponent.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class GridMovementComponent : MonoBehaviour
 {
+    public float MoveTime = 0.5f;
+    public void DoMove(Vector2 Direction)
+    {
+        Vector2 Movepoint = new Vector2(transform.position.x, transform.position.y) + Direction;
+        this.transform.DOMove(Movepoint, MoveTime).SetEase(Ease.OutExpo);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Hero/HeroControlScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroControlScript : MonoBehaviour
+{
+
+    public GridMovementComponent MovementComponent;
+
+     private void Awake()
+    {
+        if (MovementComponent == null)
+        {
+            MovementComponent = gameObject.GetComponent<GridMovementComponent>();
+        }
+    }
+
+
+    
+
+    public EMoveResult ProcessMoveEnum(EPlayerMoves InputMove)
+    {
+        switch (InputMove)
+        {
+            case EPlayerMoves.UP:
+                MovementComponent.DoMove (new Vector2(0, 1));
+                return EMoveResult.SUCCESS;
+                break;
+            case EPlayerMoves.RIGHT:
+                MovementComponent.DoMove(new Vector2(1, 0));
+                return EMoveResult.SUCCESS;
+                break;
+            case EPlayerMoves.LEFT:
+                MovementComponent.DoMove(new Vector2(-1, 0));
+                return EMoveResult.SUCCESS;
+                break;
+            case EPlayerMoves.DOWN:
+                MovementComponent.DoMove(new Vector2(0, -1));
+                return EMoveResult.SUCCESS;
+                break;
+
+            default:
+                Debug.LogError("Could not process move " + InputMove.ToString());
+                return EMoveResult.SYSTEM_ERROR;
+                break;
+        }
+
+        return EMoveResult.NEUTRAL;
+    }
+}
