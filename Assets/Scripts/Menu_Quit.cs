e74e3272a46e27f3844463b480c14737d2b8409e
DESKTOP-6QRDQEC\DavidN <DavidENishball@gmail.com>
2020-07-11 23:11:12 -0400

    Added scenes to builder.  Added temp menu.  Pressing Escape will now quit.

---
+++ Assets/Scripts/Menu_Quit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu_Quit : MonoBehaviour
+{
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
